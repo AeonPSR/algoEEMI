0c0f8d39c35a8bc738ac8ed06b98c0e6054d4e1c
Aeon <floriandevigne1@hotmail.fr>
2024-01-18 10:49:43 -0500

    Added the countLetterEach() function

--- base.py
+++ base.py
@@ -52,7 +52,22 @@ def longestName(tab):
     print("\n")
     return (tab[iLongest])
 
+def countLetterEach(tab, letter):
+    countTot = 0
+    id = 0
+    for elements in tab:
+        count = 0
+        for letters in elements:
+            if letters == letter:
+                count += 1
+        countTot += count
+        print(f"{tab[id]} has {count} times the letter {letter}.")
+        id += 1
+
+    print(f"\nThe letter {letter} is present {countTot} times in total.\n")
+    return()
+
 print (tab)
-print (f"\n{longestName(tab)}")
+print (f"\n{countLetterEach(tab, 'i')}")
 
 print(f"\nTime taken: {time.time() - start_time} seconds")
